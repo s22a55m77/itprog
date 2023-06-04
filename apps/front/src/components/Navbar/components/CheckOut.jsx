c6c68a66867ed4444d9f3106599deb3222e51df3
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-06-04 14:46:49 +0800

    add modal

--- apps/front/src/components/Navbar/components/CheckOut.jsx
+++ apps/front/src/components/Navbar/components/CheckOut.jsx
@@ -1,32 +1,43 @@
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button } from '@mui/material';
 import { useState } from 'react';
+import Modal from '@mui/material/Modal';
 
 export default function CheckOut() {
-  const [isOpen, setIsOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <Button
         variant="contained"
         color="primary"
-        style={{ padding: '3px 16px 3px 16px' }}
-        onClick={(e) => setIsOpen(e.currentTarget)}
+        style={{ padding: '3px 16px 3px 16px', whiteSpace: 'nowrap'}}
+        onClick={(e) => setIsOpen(true)}
       >
         Check Out
       </Button>
-      <Menu
-        id={"Order Menu"}
-        open={Boolean(isOpen)}
-        anchorEl={isOpen}
-        PaperProps={{
-          style: {
-            transform: 'translateY(2px)',
-          }
-        }}
+      <Modal
+        open={isOpen}
         onClose={() => setIsOpen(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        style={{display:'flex',alignItems:'center',justifyContent:'center'}}
       >
-        <MenuItem>1</MenuItem>
-      </Menu>
+        <div
+          style={{
+            width: '400px',
+            height: '500px',
+            position: 'absolute',
+            top: '50%',
+            transform: 'translate(0, -50%)',
+            margin: 'auto',
+            backgroundColor: 'white',
+            borderRadius: '10px',
+            padding: '10px',
+          }}
+        >
+
+        </div>
+      </Modal>
     </>
   )
 }
