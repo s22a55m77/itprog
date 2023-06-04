c2d232820a7c823383e56a95c7a3ec1c12082472
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-06-04 14:46:11 +0800

    fix request return

--- apps/front/src/services/api.js
+++ apps/front/src/services/api.js
@@ -1,10 +1,39 @@
+import { getCookie } from '../utils';
+
 const URL = 'https://itprog-back.azurewebsites.net/';
 
 
 export const getCategory = async () => {
-  return await fetch(URL + 'category')
+  const res = await fetch(URL + 'category')
+
+  return res.json()
 }
 
 export const getDish = async (id) => {
-  return await fetch(URL + 'dish/' + id)
+  const res = await fetch(URL + 'dish/' + id)
+  return res.json()
+}
+
+export const login = async (param) => {
+  const res = await fetch(URL + 'auth/login', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(param)
+  })
+
+  return res.json()
+}
+
+export const me = async () => {
+  const res = await fetch(URL + 'auth/me', {
+    headers: {
+      Authorization: `Bearer ${getCookie('jwtToken')}`,
+    },
+    method: 'GET',
+  })
+
+  return res.json();
+
 }
