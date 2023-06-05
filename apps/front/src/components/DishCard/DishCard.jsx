58dfe5132b9aefaafec2d2187f33f9bd9916a8bb
1Jerome23 <134996022+1Jerome23@users.noreply.github.com>
2023-06-05 12:32:47 +0800

    Update DishCard.jsx

--- apps/front/src/components/DishCard/DishCard.jsx
+++ apps/front/src/components/DishCard/DishCard.jsx
@@ -10,15 +10,36 @@ export default function DishCard ({dish}) {
   // }, [])
 
 const RestaurantCard = () => {
+  const [quantity, setQuantity] = useState(1); // State variable for quantity
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <div className="restaurant-card">
-      <img src="path_to_image" alt="Restaurant" />
-      <h2>Dish Name</h2>
-      <p className="price">$10.99</p>
-      <p className="description">Description of the dish goes here.</p>
+      <img src="path_to_image" alt="Restaurant" className="card-image" />
+      <div className="card-content">
+        <h2 className="dish-name">Dish Name</h2>
+        <p className="description">Description of the dish goes here.</p>
+        <div className="price-quantity">
+          <div className="quantity-control">
+            <button onClick={decreaseQuantity}>-</button>
+            <span>{quantity}</span>
+            <button onClick={increaseQuantity}>+</button>
+          </div>
+          <p className="price">$10.99</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
   return (
     <>
