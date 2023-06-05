de4ac415c7d92aa43c1ebe3ea6b23244425e896a
1Jerome23 <134996022+1Jerome23@users.noreply.github.com>
2023-06-05 12:06:22 +0800

    Update DishCard.jsx

--- apps/front/src/components/DishCard/DishCard.jsx
+++ apps/front/src/components/DishCard/DishCard.jsx
@@ -1,12 +1,25 @@
 import { Card, CardContent } from '@mui/material';
 import { useEffect } from 'react';
 import { getDish } from '../../services/api';
+import React from 'react';
+import './styles.css'; // Import the CSS file
 
 export default function DishCard ({dish}) {
 
   // useEffect(() => {
   //   getDish()
   // }, [])
+
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card">
+      <img src="path_to_image" alt="Restaurant" />
+      <h2>Dish Name</h2>
+      <p className="price">$10.99</p>
+      <p className="description">Description of the dish goes here.</p>
+    </div>
+  );
+}
 
   return (
     <>
