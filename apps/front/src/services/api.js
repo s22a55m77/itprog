cd82ef1f38a13d28ca6b577ff28986526a08ad5d
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-06-15 20:28:26 +0800

    add register, getOrderDetail

--- apps/front/src/services/api.js
+++ apps/front/src/services/api.js
@@ -30,8 +30,31 @@ export const login = async (param) => {
   return res.json()
 }
 
+export const register = async (param) => {
+  const res = await fetch(URL + 'auth/register', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(param)
+  })
+
+  return res.json()
+}
+
 export const me = async () => {
   const res = await fetch(URL + 'auth/me', {
+    headers: {
+      Authorization: `Bearer ${getCookie('jwtToken')}`,
+    },
+    method: 'GET',
+  })
+
+  return res.json();
+}
+
+export const getOrderDetail = async (orderNumber) => {
+  const res = await fetch(URL + 'order/' + orderNumber, {
     headers: {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
