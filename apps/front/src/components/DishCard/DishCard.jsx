707b8ce50ae28a187cf2fd14783f27e4614bb564
s22a55m77 <s22a55m77@gmail.com>
2023-06-16 09:45:42 +0800

    change image src to new endpoint

--- apps/front/src/components/DishCard/DishCard.jsx
+++ apps/front/src/components/DishCard/DishCard.jsx
@@ -7,65 +7,56 @@ import { CartContext } from '../../App';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
 import { useMount } from 'ahooks';
+import { URL } from '../../services/api';
 
-export default function DishCard ({id, active, category}) {
+export default function DishCard({ id, active, category }) {
   const [dish, setDish] = useState();
   const [quantity, setQuantity] = useState(1); // State variable for quantity
   const [image, setImage] = useState();
 
-  const cart = useContext(CartContext)
-
+  const cart = useContext(CartContext);
 
   useMount(() => {
     setQuantity(1);
 
-    getDish(id, true).then((res) => {
+    getDish(id, false).then((res) => {
       setDish(res.data);
-    })
-
-  })
+    });
+  });
 
   useEffect(() => {
-    if (dish && dish.image) {
-      const buffer = new Uint8Array(dish.image.data);
-      const decoder = new TextDecoder();
-      const decodedData = decoder.decode(buffer);
-      setImage(decodedData);
-    }
-
     if (active && active.id == id && dish) {
-      const index = cart.cart.findIndex(x => x.categoryId === category);
+      const index = cart.cart.findIndex((x) => x.categoryId === category);
       cart.cart[index].dish.dishId = id;
       cart.cart[index].dish.name = dish.name;
       cart.cart[index].dish.quantity = quantity;
       cart.cart[index].dish.price = dish.price;
-
     }
-  }, [active])
+  }, [active]);
 
   const changeCartQuantity = (quantity) => {
-    const index = cart.cart.findIndex(x => x.categoryId === category);
+    const index = cart.cart.findIndex((x) => x.categoryId === category);
     cart.cart[index].dish.quantity = quantity;
-  }
+  };
 
   const increaseQuantity = () => {
-    changeCartQuantity(quantity + 1)
+    changeCartQuantity(quantity + 1);
     setQuantity(quantity + 1);
   };
 
   const decreaseQuantity = () => {
     if (quantity > 1) {
-      changeCartQuantity(quantity - 1)
+      changeCartQuantity(quantity - 1);
       setQuantity(quantity - 1);
     }
   };
 
   const change = (data) => {
     if (data.target.value >= 1) {
-      setQuantity(data.target.value)
-      changeCartQuantity(data.target.value)
+      setQuantity(data.target.value);
+      changeCartQuantity(data.target.value);
     }
-  }
+  };
 
   return (
     <div className="restaurant-card">
@@ -75,23 +66,32 @@ export default function DishCard ({id, active, category}) {
           overflow: 'hidden',
         }}
       >
-        {image ? <img src={image} alt="Restaurant" className="card-image" /> :
-          <div
-            className={"card-image"}
-          >
-            <HideImageTwoToneIcon sx={{ fontSize: 80 }}/>
+        {image ? (
+          <img src={`${URL}dish/${id}/image`} alt="Restaurant" className="card-image" />
+        ) : (
+          <div className={'card-image'}>
+            <HideImageTwoToneIcon sx={{ fontSize: 80 }} />
           </div>
-         }
-
+        )}
       </div>
       <div className="card-content">
         <h2 className="dish-name">{dish && dish.name}</h2>
         <p className="description">{dish && dish.description}</p>
         <div className="price-quantity">
           <div className="quantity-control">
-            <button onClick={decreaseQuantity}><RemoveIcon /></button>
-            <input onChange={change} type={"number"} min={1} className={"quantity-control-input"} value={quantity}/>
-            <button onClick={increaseQuantity}><AddIcon /></button>
+            <button onClick={decreaseQuantity}>
+              <RemoveIcon />
+            </button>
+            <input
+              onChange={change}
+              type={'number'}
+              min={1}
+              className={'quantity-control-input'}
+              value={quantity}
+            />
+            <button onClick={increaseQuantity}>
+              <AddIcon />
+            </button>
           </div>
           <p className="price">₱{dish && dish.price}</p>
         </div>
--- apps/front/src/services/api.js
+++ apps/front/src/services/api.js
@@ -1,46 +1,47 @@
 import { getCookie } from '../utils';
 
-const URL = 'https://itprog-back.azurewebsites.net/';
-
+export const URL = 'https://itprog-back.azurewebsites.net/';
 
 export const getCategory = async () => {
-  const res = await fetch(URL + 'category')
+  const res = await fetch(URL + 'category');
 
-  return res.json()
-}
+  return res.json();
+};
 
 export const getDishes = async (id) => {
-  const res = await fetch(URL + `category/${id}/dishes`)
-  return res.json()
-}
+  const res = await fetch(URL + `category/${id}/dishes`);
+  return res.json();
+};
 export const getDish = async (id, isImage) => {
-  const res = await fetch(URL + `dish/${id}${isImage ? '?image=true' : ''}`, {cache: 'force-cache'})
-  return res.json()
-}
+  const res = await fetch(URL + `dish/${id}${isImage ? '?image=true' : ''}`, {
+    cache: 'force-cache',
+  });
+  return res.json();
+};
 
 export const login = async (param) => {
   const res = await fetch(URL + 'auth/login', {
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify(param)
-  })
+    body: JSON.stringify(param),
+  });
 
-  return res.json()
-}
+  return res.json();
+};
 
 export const register = async (param) => {
   const res = await fetch(URL + 'auth/register', {
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify(param)
-  })
+    body: JSON.stringify(param),
+  });
 
-  return res.json()
-}
+  return res.json();
+};
 
 export const me = async () => {
   const res = await fetch(URL + 'auth/me', {
@@ -48,10 +49,10 @@ export const me = async () => {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'GET',
-  })
+  });
 
   return res.json();
-}
+};
 
 export const getOrderDetail = async (orderNumber) => {
   const res = await fetch(URL + 'order/' + orderNumber, {
@@ -59,10 +60,10 @@ export const getOrderDetail = async (orderNumber) => {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'GET',
-  })
+  });
 
   return res.json();
-}
+};
 
 export const addOrder = async (param) => {
   const res = await fetch(URL + 'order', {
@@ -71,11 +72,11 @@ export const addOrder = async (param) => {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'POST',
-    body: JSON.stringify(param)
-  })
+    body: JSON.stringify(param),
+  });
 
-  return res.json()
-}
+  return res.json();
+};
 
 export const cancelOrder = async (param) => {
   const res = await fetch(URL + `order/${param}/cancel`, {
@@ -84,10 +85,10 @@ export const cancelOrder = async (param) => {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'POST',
-  })
+  });
 
-  return res.json()
-}
+  return res.json();
+};
 
 export const getOrder = async () => {
   const res = await fetch(URL + 'order', {
@@ -95,21 +96,20 @@ export const getOrder = async () => {
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'GET',
-  })
+  });
 
   return res.json();
-}
+};
 
-  export const addPayment = async (order, param) => {
+export const addPayment = async (order, param) => {
   const res = await fetch(URL + `order/${order}`, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${getCookie('jwtToken')}`,
     },
     method: 'POST',
-    body: JSON.stringify(param)
-  })
+    body: JSON.stringify(param),
+  });
 
-  return res.json()
-}
-
+  return res.json();
+};
