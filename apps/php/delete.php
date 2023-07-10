ecc1e1274feb82dfe3df4d6c1211ba727352dda7
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-07-10 14:26:50 +0800

    add login and delete

--- apps/php/delete.php
+++ apps/php/delete.php
@@ -1,13 +1,53 @@
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
   <title>Dish Management</title>
   <link rel="stylesheet" href="global.css">
 </head>
+
 <body>
   <!-- TODO pass the whole sql statement here using POST method -->
 
-  <!-- Confirm Modal -->
-  Body
+  <div class="navbar">
+    <div>
+      <span> Dish Management System <span>
+    </div>
+    <div>
+      username
+    </div>
+  </div>
+
+  <!-- CONTAINER -->
+  <div class="container">
+    <!-- SIDEBAR -->
+    <div class="sidebar">
+      <a class="menu" href="category.php">
+        Category
+      </a>
+      <a class="menu" href="dish.php">
+        Dish
+      </a>
+      <a class="menu" href="combo.php">
+        Combo
+      </a>
+    </div>
+    <!-- Confirm Modal -->
+    <div class="body">
+      <div class="delete-container">
+        <div class="delete-header">
+          Delete Confirm
+        </div>
+          <!-- TODO information of the deleted item -->
+        <div class="delete-content">
+          INFORMATION
+        </div>
+        <div class="delete-content">
+          <button class="error"><a href="javascript:history.back()" style="color: #fff">Cancel</a></button>
+          <button>Confirm</button>
+        </div>
+      </div>
+    </div>
 </body>
+
 </html>
---
+++ apps/php/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <title>Dish Management</title>
+  <link rel="stylesheet" href="global.css">
+</head>
+
+<body>
+  <div class="navbar">
+    <div>
+      <span> Dish Management System <span>
+    </div>
+  </div>
+
+  <!-- CONTAINER -->
+  <div class="container">
+    <!-- Confirm Modal -->
+    <div class="body">
+      <div class="login-container">
+        <div class="login-header">
+          Login Form
+        </div>
+        <div class="login-content">
+          <form action="">
+            <input placeholder="username"/>
+            <input type="password" placeholder="password"/>
+          </form>
+        </div>
+        <div class="login-content">
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
