1fb1548f95f15b661de39606fde37af0b9dce236
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-07-17 16:23:25 +0800

    add sql

--- apps/php/main.php
+++ apps/php/main.php
@@ -21,7 +21,9 @@
       <span> Dish Management System <span>
     </div>
     <div>
-      username
+        <?php
+            echo getUsername();
+        ?>
     </div>
   </div>
 
@@ -43,6 +45,37 @@
     <!-- BODY -->
     <div class="body">
       1
+        <?php
+            /*
+            SELECT
+                d.name, SUM(od.quantity) AS total_sold, current_date() AS date
+            FROM
+                orders o LEFT JOIN `order-details` od ON o.order_number=od.order_number
+                        LEFT JOIN dishes d ON od.dish_id=d.id
+            WHERE DATE(o.completed_at)=current_date() -- '2023-06-27' for test
+            GROUP BY od.dish_id
+
+
+            name   total_sold  date
+            Steak	6	2023-07-17
+            Steamed Vegetables	8	2023-07-17
+            Root Beer	8	2023-07-17
+            Salmon	2	2023-07-17
+            Chicken	2	2023-07-17
+
+            SELECT SUM((round((o.price/(1-c.discount*0.01)))-o.price))  AS total_discount
+            FROM orders o
+            JOIN combos c ON o.combo_id = c.id
+            WHERE DATE(o.completed_at)='2023-06-27';
+
+            SELECT
+                SUM(o.price) AS total_amount
+            FROM orders o
+            WHERE DATE(o.completed_at)='2023-06-27'
+            GROUP BY DATE(o.completed_at);
+
+             * */
+        ?>
     </div>
   </div>
 </body>
