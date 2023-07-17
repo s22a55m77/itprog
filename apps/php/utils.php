c3b3596486a689bd96d6950fcb01935e2f55603a
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-07-17 16:23:46 +0800

    add session guard function, get username function

--- apps/php/utils.php
+++ apps/php/utils.php
@@ -4,17 +4,25 @@
                 or die ("Unable to connect!". mysqli_error());
     $db = mysqli_select_db($conn, 'public');
 
-    function verifyLogin() {
-        global $conn;
-
-    }
-
 
     function checkLogin() {
         session_start();
         if (!isset($_SESSION['getLogin'])) {
             header("location:login.php");
         }
+    }
+
+    function getUsername() {
+        global $conn;
+
+        $usernameFromSession = $_SESSION['getLogin'];
+
+        $query = mysqli_query($conn, "SELECT username, password FROM users 
+                                           WHERE username='$usernameFromSession'");
+
+        $fetch = mysqli_fetch_array($query);
+
+        return $fetch["username"];
     }
 
 ?>
