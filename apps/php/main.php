91bf7aa3331489d9472643f7172f0fca046bebb9
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-07-18 11:34:51 +0800

    add total price, total discount, dished sale card

--- apps/php/main.php
+++ apps/php/main.php
@@ -44,7 +44,116 @@
     
     <!-- BODY -->
     <div class="body">
-      1
+      <div class="main-container">
+        <div class="card">
+            <div class="card-header">
+                Total Price
+            </div>
+            <div class="card-content" style="font-size: 40px">
+                <?php
+                    global $conn;
+                    $sql = "SELECT
+                                SUM(o.price) AS total_amount
+                            FROM orders o
+                            WHERE DATE(o.completed_at)='".date("Y-m-d")."'
+                            GROUP BY DATE(o.completed_at);";
+                    $query = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($query) > 0) {
+                        $result = mysqli_fetch_assoc($query);
+                        echo "₱".$result['total_amount'];
+                    }
+                    else {
+                        echo "N/A";
+                    }
+                ?>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                Total Discount
+            </div>
+            <div class="card-content" style="font-size: 40px">
+                <?php
+                global $conn;
+                $sql = "SELECT SUM((round((o.price/(1-c.discount*0.01)))-o.price))  AS total_discount
+                        FROM orders o
+                        JOIN combos c ON o.combo_id = c.id
+                        WHERE DATE(o.completed_at)='".date("Y-m-d")."';";
+                $query = mysqli_query($conn, $sql);
+                $result = mysqli_fetch_assoc($query);
+                if ($result['total_discount'] != null) {
+                    echo "₱".$result['total_discount'];
+                }
+                else {
+                    echo "N/A";
+                }
+                ?>
+            </div>
+        </div>
+          <div class="card">
+              <div class="card-header">
+                  Total Dished Sold
+              </div>
+              <div class="card-content" style="font-size: 40px">
+                <?php
+                  global $conn;
+                  $total = 0;
+                  $sql = "SELECT
+                            d.name, SUM(od.quantity) AS total_sold, current_date() AS date
+                        FROM
+                            orders o LEFT JOIN `order-details` od ON o.order_number=od.order_number
+                                    LEFT JOIN dishes d ON od.dish_id=d.id
+                        WHERE DATE(o.completed_at)=current_date()
+                        GROUP BY od.dish_id";
+                  $query = mysqli_query($conn, $sql);
+                  if (mysqli_num_rows($query) > 0) {
+                    while ($row = mysqli_fetch_assoc($query)) {
+                        $total += 1;
+                    }
+                    echo $total;
+                  }
+                  else {
+                    echo "N/A";
+                  }
+                ?>
+              </div>
+          </div>
+      </div>
+        <div class="card" style="margin-top: 10px; margin-left: 15px; width: calc(100% - 15px);">
+            <div class="card-header">
+                Dishes Sold
+            </div>
+            <div style="font-size: 40px;">
+              <?php
+                global $conn;
+                $sql = "SELECT
+                            d.name, SUM(od.quantity) AS total_sold, current_date() AS date
+                        FROM
+                            orders o LEFT JOIN `order-details` od ON o.order_number=od.order_number
+                                    LEFT JOIN dishes d ON od.dish_id=d.id
+                        WHERE DATE(o.completed_at)=current_date()
+                        GROUP BY od.dish_id";
+                $query = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($query) > 0) {
+                  echo "<table class='table'>";
+                  echo "<tr class='table-header'>";
+                  echo "<th>Name</th>";
+                  echo "<th>Quantity</th>";
+                  echo "</tr>";
+                  while ($row = mysqli_fetch_assoc($query)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['name'] . "</td>";
+                    echo "<td>" . $row['total_sold'] . "</td>";
+                    echo "</tr>";
+                  }
+                  echo "</table>";
+                }
+                else {
+                  echo "<div class='card-content'> N/A </div>";
+                }
+              ?>
+            </div>
+        </div>
         <?php
             /*
             SELECT
@@ -55,13 +164,13 @@
             WHERE DATE(o.completed_at)=current_date() -- '2023-06-27' for test
             GROUP BY od.dish_id
 
-
             name   total_sold  date
             Steak	6	2023-07-17
             Steamed Vegetables	8	2023-07-17
             Root Beer	8	2023-07-17
             Salmon	2	2023-07-17
             Chicken	2	2023-07-17
+
 
             SELECT SUM((round((o.price/(1-c.discount*0.01)))-o.price))  AS total_discount
             FROM orders o
