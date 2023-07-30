78127ecb32cfee2c27e23727204370159f8c273d
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-07-30 13:02:50 +0800

    fix xml add

--- apps/php/dishAddXML.php
+++ apps/php/dishAddXML.php
@@ -12,90 +12,115 @@ checkLogin();
 
 
 <?php
-  if(isset($_POST['file']) && !empty($_POST['file'])) {
+  if(isset($_FILES['file']) && !empty($_FILES['file'])) {
     $xml = new DOMDocument();
-    $xml->load($_POST['file']);
+    $xml->loadXML(file_get_contents($_FILES['file']["tmp_name"]));
     if(!$xml->schemaValidate('menu.xsd')) {
       // xml not valid
       header("location:dishAddXML.php?error=1");
     }
     else {
       // xml valid
-      $Menu = simplexml_load_file($_POST['file']);
+      $Menu = simplexml_load_string(file_get_contents($_FILES['file']["tmp_name"]));
+      //validate category
+      $error = false;
       foreach ($Menu->Category as $Category) {
-        if($Category['exist'] == 'true') {
+        if ($Category['exist'] == 'true') {
           // category exist
           global $conn;
           // get category id
-          $sql = "SELECT id FROM categories WHERE name = '".$Category['name']."'";
+          $sql = "SELECT id FROM categories WHERE name = '" . $Category['name'] . "'";
           $query = mysqli_query($conn, $sql);
           $result = mysqli_fetch_object($query);
           $categoryId = $result->id;
-
-          foreach ($Category->Dish as $Dish) {
-            $name = $Dish->name;
-            $price = $Dish->price;
-            $description = $Dish->description;
-            $image = $Dish->image;
-            // insert
-            $sql = "INSERT INTO dishes(category_id, name, description, price, image) 
-                    VALUES ('$categoryId', '$name', '$description', '$price', '$image');";
+          if (mysqli_affected_rows($conn) == 0) {
+            $error = true;
+            header("location:dishAddXML.php?error=5");
+            return;
+          }
+        }elseif ($Category['exist'] == 'false') {
+            // category not exist
+            global $conn;
+            //test category not exist
+            // get category id
+            $sql = "SELECT id FROM categories WHERE name = '" . $Category['name'] . "'";
             $query = mysqli_query($conn, $sql);
-            // insert failed
-            if(mysqli_affected_rows($conn) < 1) {
-              header("location:dishAddXML.php?error=2");
+            $result = mysqli_fetch_object($query);
+            $categoryId = $result->id;
+            echo $categoryId;
+            if (mysqli_affected_rows($conn) > 0) {
+              $error = true;
+              header("location:dishAddXML.php?error=4");
               return;
             }
           }
-        }
-        elseif($Category['exist'] == 'false') {
-          // category not exist
-          global $conn;
-          //test category not exist
-          // get category id
-          $sql = "SELECT id FROM categories WHERE name = '".$Category['name']."'";
-          $query = mysqli_query($conn, $sql);
-          $result = mysqli_fetch_object($query);
-          $categoryId = $result->id;
-          echo $categoryId;
-          if(mysqli_affected_rows($conn) > 0) {
-            header("location:dishAddXML.php?error=4");
-            return;
-          }
-
-          // create category
-          $categoryName = $Category['name'];
-          $sql = "INSERT INTO categories(name) VALUES ('$categoryName') ";
-          $query = mysqli_query($conn, $sql);
-          if(mysqli_affected_rows($conn) < 1) {
-            header("location:dishAddXML.php?error=2");
-            return;
-          }
-
-          // get category id
-          $sql = "SELECT id FROM categories WHERE name = '".$Category['name']."'";
-          $query = mysqli_query($conn, $sql);
-          $result = mysqli_fetch_object($query);
-          $categoryId = $result->id;
-
-          foreach ($Category->Dish as $Dish) {
-            $name = $Dish->name;
-            $price = $Dish->price;
-            $description = $Dish->description;
-            $image = $Dish->image;
-            // insert
-            $sql = "INSERT INTO dishes(category_id, name, description, price, image) 
-                    VALUES ('$categoryId', '$name', '$description', '$price', '$image');";
-            $query = mysqli_query($conn, $sql);
-            // insert failed
-            if(mysqli_affected_rows($conn) < 1) {
-              header("location:dishAddXML.php?error=2");
-              return;
-            }
-          }
-        }
       }
-     header("location:dishAddXML.php?success=1");
+      if(!$error) {
+        // insert
+        foreach ($Menu->Category as $Category) {
+          if($Category['exist'] == 'true') {
+            // category exist
+            global $conn;
+            // get category id
+            $sql = "SELECT id FROM categories WHERE name = '".$Category['name']."'";
+            $query = mysqli_query($conn, $sql);
+            $result = mysqli_fetch_object($query);
+            $categoryId = $result->id;
+
+            foreach ($Category->Dish as $Dish) {
+              $name = $Dish->name;
+              $price = $Dish->price;
+              $description = $Dish->description;
+              $image = $Dish->image;
+              // insert
+              $sql = "INSERT INTO dishes(category_id, name, description, price, image) 
+                    VALUES ('$categoryId', '$name', '$description', '$price', '$image');";
+              $query = mysqli_query($conn, $sql);
+              // insert failed
+              if(mysqli_affected_rows($conn) < 1) {
+                header("location:dishAddXML.php?error=2");
+                return;
+              }
+            }
+          }
+          elseif($Category['exist'] == 'false') {
+            // category not exist
+            global $conn;
+
+            // create category
+            $categoryName = $Category['name'];
+            $sql = "INSERT INTO categories(name) VALUES ('$categoryName') ";
+            $query = mysqli_query($conn, $sql);
+            if(mysqli_affected_rows($conn) < 1) {
+              header("location:dishAddXML.php?error=2");
+              return;
+            }
+
+            // get category id
+            $sql = "SELECT id FROM categories WHERE name = '".$Category['name']."'";
+            $query = mysqli_query($conn, $sql);
+            $result = mysqli_fetch_object($query);
+            $categoryId = $result->id;
+
+            foreach ($Category->Dish as $Dish) {
+              $name = $Dish->name;
+              $price = $Dish->price;
+              $description = $Dish->description;
+              $image = $Dish->image;
+              // insert
+              $sql = "INSERT INTO dishes(category_id, name, description, price, image) 
+                    VALUES ('$categoryId', '$name', '$description', '$price', '$image');";
+              $query = mysqli_query($conn, $sql);
+              // insert failed
+              if(mysqli_affected_rows($conn) < 1) {
+                header("location:dishAddXML.php?error=2");
+                return;
+              }
+            }
+          }
+        }
+        header("location:dishAddXML.php?success=1");
+      }
     }
   } else {
       if(isset($_POST['submit'])) {
@@ -156,7 +181,7 @@ checkLogin();
       Upload XML
     </div>
     <div class="add-container" style="padding: 10px">
-      <form action="dishAddXML.php" method="POST">
+      <form action="dishAddXML.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="file" />
         <button type="submit" style="margin-top: 5px" name="submit">Upload</button>
       </form>
@@ -175,6 +200,9 @@ checkLogin();
         }
         elseif ($error==4) {
           echo "<div class='alert'>XML is not valid! Category already exist<br/></div>";
+        }
+        elseif ($error==5) {
+          echo "<div class='alert'>XML is not valid! Category not exist, but exist is true<br/></div>";
         }
       }
       if(isset($_GET["success"])) {
